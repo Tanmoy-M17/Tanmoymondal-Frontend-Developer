cedc9a2d0e6a494ce1dc21b65834e0598eba1ec4
Tanmoy-M17 <97627356+Tanmoy-M17@users.noreply.github.com>
2022-12-16 20:44:56 +0530

    Compleated

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+  <div className="max-h-100 border-solid border-2 ">
+    <img className="max-h-full w-full" src="https://wallpaperaccess.com/full/2025987.jpg" alt=""/>
+  </div>
+  );
+};
+
+export default Banner;
---
+++ src/components/GridPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Modal from './Modal';
+
+
+const GridPage = ({usesData}) => {
+   
+  return (
+    <div className='grid gap-4 grid-cols-3'>
+    {usesData.slice(0,10).map((e,i)=>{
+            return  <Modal key={i} data={e} />
+        })}
+        <div>
+        </div>
+    </div>
+  )
+}
+
+export default GridPage
